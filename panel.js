875a3dc7b667401eab6dbd48a86a4189a1e31c47
Raqib Zaman <raqib.zaman@gmail.com>
2025-07-28 01:45:49 -0400

    This is fine, I'll pivot to add like a little menu that pops up to autofill input elements by choice or something like that.

--- panel.js
+++ panel.js
@@ -1,25 +1,17 @@
 const notepad = document.getElementById('notepad');
 const download_btn = document.getElementById('download');
-const notesWrapper = document.getElementById('notes-wrapper');
 
-const NUM_NOTES = 2;
-
-for (let i=0; i < NUM_NOTES; i++){
-    makeNoteBlock(i);
-}
-
-function makeNoteBlock(index) {
-    
-
-}
-
-// Load saved notes???
+// Load Notes
 chrome.storage.local.get("note", (data) => {
     notepad.value = data.note || "";
 });
 
+notepad.addEventListener('input', () => {
+    chrome.storage.local.set({ note: notepad.value });
+});
 
 
+// Download Notes
 download_btn.addEventListener("click", () => {
     const blob = new Blob([notepad.value], {type: "text/plain"});
     const url = URL.createObjectURL(blob);
