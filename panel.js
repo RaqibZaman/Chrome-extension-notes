e6d942a5e19b254966270d70b47d2a64826a20a8
Raqib Zaman <raqib.zaman@gmail.com>
2025-08-02 01:15:06 -0400

    Made the button generation into a function, and fixed a bug where calling this new function multiple times would cause multiple event listeners to attach to the same button. Also dynamically insert an hr element for readability

--- panel.js
+++ panel.js
@@ -1,43 +1,83 @@
 // dynamically make buttons using private data
-fetch(chrome.runtime.getURL('private.json'))
-.then(response => response.json())
-.then(data => {
-    const dyBtnGrp = document.getElementById('dynamic-btn-group');
-    data.forEach(group => {
-        const groupDiv = document.createElement('div');
-        groupDiv.className = 'btnbox';
+function generateButtons(jsonFile, groupId){
+    fetch(chrome.runtime.getURL(jsonFile))
+    .then(response => response.json())
+    .then(data => {
+        const dyBtnGrp = document.getElementById(groupId);
+        data.forEach(group => {
+            const groupDiv = document.createElement('div');
+            groupDiv.className = 'btnbox';
 
-        for (const [key, val] of Object.entries(group)) {
-            const btn = document.createElement('button');
-            btn.className = 'cpy';
-            btn.innerHTML = key;
-            btn.setAttribute('data-text', val);
-            groupDiv.appendChild(btn);
-        }
+            for (const [key, val] of Object.entries(group)) {
+                const btn = document.createElement('button');
+                btn.className = 'cpy';
+                btn.innerHTML = key;
+                btn.setAttribute('data-text', val);
+                
+                btn.addEventListener('click', () => {
+                let str = btn.getAttribute('data-text');
+                navigator.clipboard.writeText(str)
+                    .then(() => {
+                        const preTxt = btn.innerHTML;
+                        btn.innerHTML = "Copied!";
+                        setTimeout(() => {
+                            btn.innerHTML = preTxt;
+                        }, 1000);
+                    })
+                    .catch(err => console.error("copy failed", err));
+                });
 
-        dyBtnGrp.appendChild(groupDiv);
-    });
+                groupDiv.appendChild(btn);
+            }
 
-    // copy link/text from buttons to clipboard
-    document.querySelectorAll('.cpy').forEach((btnX) => {
-        btnX.addEventListener('click', () => {
-            let str = btnX.getAttribute('data-text');
-
-            navigator.clipboard.writeText(str)
-                .then(() => {
-                    const preTxt = btnX.innerHTML;
-                    btnX.innerHTML = "Copied!";
-                    setTimeout(() => {
-                        btnX.innerHTML = preTxt;
-                    }, 1000);
-                })
-                .catch(err => console.error("copy failed", err));
+            dyBtnGrp.appendChild(groupDiv);
         });
+
+        const hr = document.createElement('hr');
+        dyBtnGrp.parentNode.insertBefore(hr, dyBtnGrp.nextElementSibling);
     });
+}
+
+generateButtons('private.json', 'dynamic-btn-group-private');
+generateButtons('public.json', 'dynamic-btn-group-public');
+
+// fetch(chrome.runtime.getURL('private.json'))
+// .then(response => response.json())
+// .then(data => {
+//     const dyBtnGrp = document.getElementById('dynamic-btn-group');
+//     data.forEach(group => {
+//         const groupDiv = document.createElement('div');
+//         groupDiv.className = 'btnbox';
+
+//         for (const [key, val] of Object.entries(group)) {
+//             const btn = document.createElement('button');
+//             btn.className = 'cpy';
+//             btn.innerHTML = key;
+//             btn.setAttribute('data-text', val);
+//             groupDiv.appendChild(btn);
+//         }
+
+//         dyBtnGrp.appendChild(groupDiv);
+//     });
+
+//     // copy link/text from buttons to clipboard
+//     document.querySelectorAll('.cpy').forEach((btnX) => {
+//         btnX.addEventListener('click', () => {
+//             let str = btnX.getAttribute('data-text');
+
+//             navigator.clipboard.writeText(str)
+//                 .then(() => {
+//                     const preTxt = btnX.innerHTML;
+//                     btnX.innerHTML = "Copied!";
+//                     setTimeout(() => {
+//                         btnX.innerHTML = preTxt;
+//                     }, 1000);
+//                 })
+//                 .catch(err => console.error("copy failed", err));
+//         });
+//     });
     
-});
-
-
+// });
 
 
 const notepad = document.getElementById('notepad');
