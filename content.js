dcdcfbe3db6a69c73bd923893b5de32c8e4f6657
Raqib Zaman <raqib.zaman@gmail.com>
2025-09-10 22:39:07 -0400

    So with webpage DOM access, I am looking into programmatically collecting the dom nodes so that I can automatically fill the form

--- content.js
+++ content.js
@@ -1,7 +1,20 @@
 // trigger: manifest.json => content_scripts
-// I should be able to interact with the DOM from here
+// I should be able to interact with the webpage DOM from here
 // 
-console.log("✅ content.js injected:", location.href);
+console.log("✅ content.js injected:");
+console.log("URL href", location.href);
+console.log("URL origin", location.origin);
+console.log("URL hostname", location.hostname);
+console.log("URL pathname", location.pathname);
 console.log("hello world!");
 console.log("hello world!");
-console.log("hello world!");
+
+// looks like I can choose where the script will be active by hostname as primary factor
+if (location.hostname === "us.smartapply.indeed.com"){
+    console.log("pass!");
+    // collect the divs by indeed id
+    const divs = document.querySelectorAll('div[id^="q_"]');
+    console.log(divs);
+}
+
+// another note: I need the content.js to re-execute upon url refresh. Maybe an listener or something I might need to change in manifest...
