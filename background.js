48732a15aa68d5cbb09f4d51e172a6b5c6bc3bc1
Raqib Zaman <raqib.zaman@gmail.com>
2025-09-13 17:57:49 -0400

    Removed throttle on content.js, and added debounce feature to background.js. Seems to be working pretty well! Let's my next goal is to view the active element in the popup.js? Or somewhere

--- background.js
+++ background.js
@@ -27,15 +27,47 @@
 
 // manifest.json <= added webNavigation permission
 // when URL changes, notify tab (content.js)
-const lastUrlByTab = new Map();     // keep track of multiple tabs in chrome, check which tab has the url-changed
+// const lastUrlByTab = new Map();     // keep track of multiple tabs in chrome, check which tab has the url-changed
+
+// chrome.webNavigation.onHistoryStateUpdated.addListener(({tabId, url}) => {
+//     if (lastUrlByTab.get(tabId) === url){   // compare tab url with changed url
+//         return; // skip on duplication
+//     }
+//     lastUrlByTab.set(tabId, url); // build map on success for future reference
+//     //console.log(typeof{tabId});
+//     console.log("Background.js");
+//     console.log(url);
+//     chrome.tabs.sendMessage(tabId, {type: "URL_CHANGED", aurl: url});
+// });
+
+const lastUrlByTab = new Map(); // map the url for each tab- like an array for each tab. Good because now I can run the script per each tab
+const debounceTimers = new Map();
+const DEBOUNCE_MS = 250;
 
 chrome.webNavigation.onHistoryStateUpdated.addListener(({tabId, url}) => {
-    if (lastUrlByTab.get(tabId) === url){   // compare tab url with changed url
-        return; // skip on duplication
+    // If url is the same as before, skip... maybe runs on refresh???
+    if (lastUrlByTab.get(tabId) === url) {
+        return;
     }
-    lastUrlByTab.set(tabId, url); // build map on success for future reference
-    //console.log(typeof{tabId});
-    console.log("Background.js");
-    console.log(url);
-    chrome.tabs.sendMessage(tabId, {type: "URL_CHANGED", aurl: url});
+
+    // Update the last observed URL per tab
+    lastUrlByTab.set(tabId, url);
+
+    // Clear any existing timer for this tab - so once debounceTimer has _ _, should not run again?
+    // prevents previous timer function to fire, theoretically preventing multiple messages
+    // and preventing stale messages to fire I suppose...
+    if (debounceTimers.has(tabId)) {
+        clearTimeout(debounceTimers.get(tabId));
+    }
+
+    // Set a new debounce timer
+    const timerId = setTimeout(() => {
+        // sends only the latest URL after debounce period
+        chrome.tabs.sendMessage(tabId, {type: "URL_CHANGED", aurl: url});
+
+        // cleanup
+        debounceTimers.delete(tabId);
+    }, DEBOUNCE_MS);
+
+    debounceTimers.set(tabId, timerId);
 });
--- content.js
+++ content.js
@@ -10,14 +10,6 @@ console.log("URL pathname", location.pathname);
 console.log("hello world!");
 console.log("hello world!");
 
-// looks like I can choose where the script will be active by hostname as primary factor
-if (location.hostname === "us.smartapply.indeed.com"){
-    console.log("pass!");
-    // collect the divs by indeed id
-    const divs = document.querySelectorAll('div[id^="q_"]');
-    console.log(divs);
-}
-
 // another note: I need the content.js to re-execute upon url refresh. Maybe an listener or something I might need to change in manifest...
 
 // use addEventListener to check for page load
@@ -29,18 +21,34 @@ if (location.hostname === "us.smartapply.indeed.com"){
 
 // the issues is that because a lot of websites are SPAs and use url redirects without refreshing the page bc of client side rendering (CSR), through multiple urls, this listener may trigger twice after going to the "next page" once.
 // to fix this, I'll use setTime to conditionally run the inner function after .25 seconds
-let runScript = true;
+// let runScript = true;
 chrome.runtime.onMessage.addListener((m) => {
-    if (runScript) {
-        runScript = false;
-        setTimeout(() => {
-            runScript = !runScript;
-        }, 250);
+    // I need to wait before checking the DOM
+    // if (runScript) {
+    //     runScript = false;
+    //     setTimeout(() => {
+    //         runScript = !runScript;
+    //     }, 250);
         if (m?.type === "URL_CHANGED") {
             console.log(m.aurl);
             console.log("testing123");
             console.log(Date());
+            // looks like I can choose the url script will be active by hostname as primary factor
+            if (location.hostname === "us.smartapply.indeed.com"){
+                console.log("pass!");
+                // check if the script is on the right page
+                console.log(location.href);
+                // collect the divs by indeed id
+                const divs = document.querySelectorAll('div[id^="q_"]');
+                console.log(divs);
+            }
+            // I need a live expression to check what is selected/clicked. So I know what id to refer to when automating
+            // use: document.activeElement
+
+
+            // replicate user click 
+
         }
 
-    }
+    // }
 });
