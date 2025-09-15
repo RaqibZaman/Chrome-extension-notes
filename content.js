ff9a6d44fcf4b0658601e5a767374ca2bfae1235
Raqib Zaman <raqib.zaman@gmail.com>
2025-09-15 13:29:42 -0400

    Was able to automatically fill inputs according to labels, nice! Now need a way to add entries via localStorage. I might look into indexDB, but for the amount of data I'm working with that is overkill for now

--- content.js
+++ content.js
@@ -41,16 +41,14 @@ chrome.runtime.onMessage.addListener((m) => {
             // looks like I can choose the url script will be active by hostname as primary factor
             if (location.hostname === "us.smartapply.indeed.com"){
                 console.log("pass!");
-                // check if the script is on the right page
-                console.log(location.href);
-                // collect the divs by indeed id
-                const divs = document.querySelectorAll('div[id^="q_"]');
-                console.log(divs);
-                divs.forEach(div => {
-                    const inputs = div.querySelectorAll('input');
-                    console.log(`${div.id} inputs: `, inputs);
-                });
+                console.log(location.href); // check page
+                
+                // global check for inputs
+                const inputs = document.querySelectorAll('input');
+                console.log(inputs);
+
                 hardCode();
+                indeed_module_querySelect(); // by div id "q_"
             }
             
 
@@ -68,8 +66,52 @@ function hardCode() {
             document.querySelector('[data-testid="resume-selection-file-resume-radio-card-input"]').click();
             document.querySelector('[data-testid="continue-button"]').click();
             console.log("hardCode");
-        }, 2000);
+        }, 400);
     }
 }
 
-// in a div, find the input elements and associated label, auto populate information, need to store information, use functions and don't try to hard-code please.
+function indeed_module_querySelect(){
+    console.log('hello!');
+    // collect the divs by indeed id "q_"
+    // note that if there are no divs in with id format q_, you will not get anything
+    const divs = document.querySelectorAll('div[id^="q_"]');
+    divs.forEach(div => {
+        const inputs = div.querySelectorAll('input');
+        console.log(`${div.id} inputs: `, inputs);
+        
+    });
+
+    
+    // find label with attribute for="x"
+    const labelsWithForAtt = document.querySelectorAll('label[for]');
+    console.log("labelForVals: ", labelsWithForAtt);
+    // go through each label, get associated input element with id="x"
+    labelsWithForAtt.forEach(label => {
+        const LabelForVal = label.getAttribute('for');
+        const LabelText = label.innerHTML;
+        console.log("forVal", LabelForVal);
+        console.log("label text", LabelText);
+        // if the inner html of label matches what we have, even partially, fill in value to associated input
+        const input = document.getElementById(LabelForVal)
+        console.log("Associated Input: ", input);
+
+        // check if input is text type
+        if (input.type == "text"){
+            // check if forVal matches
+            if (LabelText in formFill.text){
+                input.value = formFill.text[LabelText];
+            }
+        }
+
+
+
+    });
+}
+
+// "hard" test data
+const formFill = {
+    text: {
+        "Job title": "Full Stack Developer",
+        "Company": "Customer Value Partners (CVP)"
+    }
+};
